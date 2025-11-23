0feadb03ec75fad29710c556bbd2da82efc88156
johe00123 <kita92666@gmail.com>
2025-11-23 19:47:54 +0900

    chore: 쉼표 누락된것 추가 #34

--- backend/main.py
+++ backend/main.py
@@ -21,8 +21,8 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:3000",
         "http://127.0.0.1:5173",
-        "http://34.239.248.132:3000"
-        "http://34.239.248.132:5173"
+        "http://34.239.248.132:3000",
+        "http://34.239.248.132:5173",
     ],
     allow_credentials=True,
     allow_methods=["GET", "POST", "DELETE", "OPTIONS"],  # 실제 사용하는 메서드만 허용
