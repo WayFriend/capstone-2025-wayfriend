afaf690ba09fd9bc8ec7abbca851feee65609fcf
leejoohee <121540336+johe00123@users.noreply.github.com>
2025-11-23 19:17:31 +0900

    Feat/walk path penalty fix-34-ec2 주소 추가 (#40)
    * Feat/route pathfinding v2-28 (#29) (#31)
    
    * fix: Docker 환경에서 main.py import 경로 수정 #26
    
    * feat: 경로 회피 로직 개선 및 최종 경로 계산 기능 추가 #28
    
    * chore: scikit-learn==1.3.2 라이브러리 추가 #30
    
    * chore: requirements.txt 설치중 오류로 인해 수정 #30
    
    * fix: 차로로 나오는 오류를 해결하기위한 패널티 때리기 버전 #34
    
    * feat: ox.settings의 캐시 임시로 false 처리 #34
    
    * feat: AWS EC2주소 추가 #34

--- backend/map_api.py
+++ backend/map_api.py
@@ -21,6 +21,7 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:3000",
         "http://127.0.0.1:5173"
+        "http://34.239.248.132:3000"
     ],  # 프론트엔드 URL
     allow_credentials=True,
     allow_methods=["*"],
